1b480f32a640e0933d21d9189c2beb2db6c159eb
RianaFerreira <rianaferreira@gmail.com>
2016-10-09 16:17:53 +1000

    Use axios library to request data from the OpenWeatherMap api using promises to wait for the response

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=9974208d09a362b7ece2090c43aefde3&units=metric';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+
+    // ES6 template strings `${inject variables inside a string}`
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (response) {
+      if (response.data.cod && response.data.message) {
+        throw new Error(response.data.message);
+      } else {
+        return response.data.main.temp;
+      }
+    }, function (response) {
+      throw new Error(response.data.message);
+    })
+  }
+}
